6e9d44c48cba62bf746c215c96983195c2f686ee
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-04-03 22:08:18 -0700

    cleanup and bug fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,8 +41,8 @@ module.exports = () => {
         description: 'Create notes or code snippets with or without internet connection!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
